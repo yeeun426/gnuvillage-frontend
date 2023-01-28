596754ede75020dedf817b0c1031d7f6a10ce094
yeeun426 <thsudkcla7@naver.com>
2023-01-28 17:04:32 +0900

    활동사진 결과물 채우기

--- src/pages/StudyResults.js
+++ src/pages/StudyResults.js
@@ -6,7 +6,31 @@ import styled from "styled-components";
 import { IntroductionStyle } from "../styles/styled";
 import StudyResultItem from "../components/StudyResultItem";
 
-const StudyResultsStyle = styled.div``;
+import study1 from "../assets/images/study_image_1.jpg";
+import study2 from "../assets/images/study_image_2.jpg";
+import study3 from "../assets/images/study_image_12.jpg";
+import study5 from "../assets/images/study_image_9.jpg";
+import study7 from "../assets/images/study_image_7.jpg";
+import study8 from "../assets/images/study_image_8.jpg";
+
+import result1 from "../assets/images/result_웹개발.JPG"
+import result2 from "../assets/images/result_데이터.png"
+import result3 from "../assets/images/result_java.png"
+import result4 from "../assets/images/result_웹.jpg"
+
+const StudyResultsStyle = styled.div`
+  .swiper-slide img {
+    width: 300px;
+  }
+
+  .swiper-button-prev {
+    left: 0;
+  }
+  
+  .swiper-button-prev {
+    right: 0;
+  }
+`;
 
 const StudyResultsPage = styled.div`
   position: relative;
@@ -19,6 +43,7 @@ const Pos = styled.div`
   align-content: flex-start;
   justify-content: flex-start;
   flex-flow: row wrap;
+  flex-wrap: wrap;
 `;
 
 const StudyTitle1 = styled.div`
@@ -47,35 +72,47 @@ export default function StudyResults() {
 
       <StudyResultsPage>
         <Pos>
-          <Link to="/study-intro/0" style={{ color: "black" }}>
+          {/* <Link to="/study-intro/0" style={{ color: "black" }}>
             <StudyResultItem></StudyResultItem>
-          </Link>
-          <StudyResultItem></StudyResultItem>
-          <StudyResultItem></StudyResultItem>
-          <StudyResultItem></StudyResultItem>
-          <StudyResultItem></StudyResultItem>
-          <StudyResultItem></StudyResultItem>
+          </Link> */}
+          <StudyResultItem 
+            study={"Web development"}
+            image = {result1}>
+          </StudyResultItem>
+          <StudyResultItem 
+            study={"Data science"}
+            image = {result2}>
+          </StudyResultItem>
+          <StudyResultItem
+            study={"Algorithms"}
+            image = {result3}>
+          </StudyResultItem>
+          <StudyResultItem
+            study={"Web basic"}
+            image = {result4}>
+          </StudyResultItem>
+          {/* <StudyResultItem></StudyResultItem> */}
         </Pos>
 
         <StudyTitle1>활동 사진</StudyTitle1>
-        <Swiper spaceBetween={30} slidesPerView={4} navigation loop={true}>
+        <Swiper spaceBetween={0} slidesPerView={4} navigation loop={true}>
           <SwiperSlide>
-            <StudyResultItem></StudyResultItem>
+            <img src={study1} alt="study1"/>
           </SwiperSlide>
           <SwiperSlide>
-            <StudyResultItem></StudyResultItem>
+            <img src={study2} alt="study1"/>
           </SwiperSlide>
           <SwiperSlide>
-            <StudyResultItem></StudyResultItem>
+            <img src={study3} alt="study1"/>
           </SwiperSlide>
           <SwiperSlide>
-            <StudyResultItem></StudyResultItem>
+            <img src={study5} alt="study1"/>
           </SwiperSlide>
           <SwiperSlide>
-            <StudyResultItem></StudyResultItem>
+            <img src={study7} alt="study1"/>
           </SwiperSlide>
           <SwiperSlide>
-            <StudyResultItem></StudyResultItem>
+            <img src={study8} alt="study1"/>
           </SwiperSlide>
         </Swiper>
       </StudyResultsPage>
